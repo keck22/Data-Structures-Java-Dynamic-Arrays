bf1e2b56a491ef70d7b11e251d773edbb0648099
keck22 <66656015+keck22@users.noreply.github.com>
2022-02-13 11:09:25 -0500

    Create CustomArray.java

---
+++ CustomArray.java
@@ -0,0 +1,9 @@
+public class CustomArray {
+    
+    public static void main(String []args){
+        Array custom = new Array(5);
+        custom.printArray();
+        System.out.println("Caleb is Here");
+     }
+    
+}
